cd287b6fe8ac29bd4090c09fc7554465487bbf4e
VANTRONG <tvtrong12a239@gmail.com>
2021-04-18 13:26:57 +0700

    :

--- src/models/index.js
+++ src/models/index.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 const Comment = required('./comment.js');
 const Favourite = required('./favourite.js');
 const Notification = required('./notification.js');
@@ -14,4 +15,26 @@ module.exports = {
     PostCategory,
     Post,
     UserRole,
+=======
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+    username: { type: String, unique: true, index: true, require: true  },
+    password: { type: String, select: false, required: true, minlength:[6, "password more than 6 degits"] },
+    password_hash: { type: String, select: false, required: true },
+    full_name:{type: String, unique: false, default: ''},
+    birthday: {type: Date, default: ''},
+    email: {type: String, unique: true, index: true, required: true},
+    student_id: {type: String, unique: true, require: true},
+    role: {type: String, require: true},
+    is_block: {type: Boolean, default: ''}
+});
+
+const User = mongoose.model('users', userSchema);
+
+module.exports = {
+    User,
+>>>>>>> Stashed changes
 }
